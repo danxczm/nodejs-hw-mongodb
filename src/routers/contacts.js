159493709d5cb989a9963c3579e44f4aad39308a
dan <81313913+danxczm@users.noreply.github.com>
2025-08-11 19:41:29 +0300

    feat: validation added

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -11,16 +11,28 @@ import {
   patchNewContactController,
   deleteContactController,
 } from '../controllers/contacts.js';
+import { createContactSchema, updateContactSchema } from '../validations/contacts.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { isValidId } from '../middlewares/isValidId.js';
 
 const router = Router();
 
 router.get('/contacts', AsyncControlWrapper(getAllContactsListController));
 
-router.get('/contacts/:contactId', AsyncControlWrapper(getContactByIdController));
+router.get('/contacts/:contactId', isValidId, AsyncControlWrapper(getContactByIdController));
 
-router.post('/contacts', AsyncControlWrapper(createNewContactController));
+router.post(
+  '/contacts',
+  validateBody(createContactSchema),
+  AsyncControlWrapper(createNewContactController)
+);
 
-router.patch('/contacts/:contactId', AsyncControlWrapper(patchNewContactController));
+router.patch(
+  '/contacts/:contactId',
+  isValidId,
+  validateBody(updateContactSchema),
+  AsyncControlWrapper(patchNewContactController)
+);
 
 router.delete('/contacts/:contactId', AsyncControlWrapper(deleteContactController));
 
